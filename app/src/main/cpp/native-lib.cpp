15584f8f362df18885a5654531201312f1f9963e
hyunJIN7 <yunjingong12@naver.com>
2025-12-24 15:29:57 +0900

    [김현진, SolarSystem] [변경 사항] - 목표 : EGL + ES 3.0 context 생성 - EGL 초기화 - Surface 받아서 EGL 붙이고 - glClearColor로 화면 색 칠하기 1. SurfaceView 생성 2. surfaceViewCreate() 호출 3. JNI 진입 4. ANativeWindow 획득 5. EGLDisplay 초기화 6. ES 3.0 Context 생성 7. Surface 와 Context 연결 8. glClearColor - 이 단계까진 렌더 루프, 쉐이더, 삼각형 없음

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,18 +1,65 @@
 #include <jni.h>
-#include <string>
+#include <android/native_window_jni.h>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_solarsystem_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
-}
+#include <EGL/egl.h>
+#include <GLES3/gl3.h>
+
+// 상태 유지 용 전역 변수
+static EGLDisplay eglDisplay = EGL_NO_DISPLAY;
+static EGLSurface eglSurface = EGL_NO_SURFACE;
+static EGLContext eglContext = EGL_NO_CONTEXT;
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_example_solarsystem_MainActivity_onSurfaceCreated(JNIEnv *env, jobject thiz,
                                                            jobject surface) {
-    // TODO: implement onSurfaceCreated()
+    // Java Surface 객체에서 ANativeWindow 생성
+    ANativeWindow* window = ANativeWindow_fromSurface(env,surface);
+
+    // EGLDisplay 초기화 (GPU 연결)
+    eglDisplay = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+    eglInitialize(eglDisplay, nullptr, nullptr);
+
+    // EGLConfig 설정(픽셀 포맷)
+    const EGLint configAttribs[] = {
+            EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT,
+            EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+            EGL_RED_SIZE, 8,
+            EGL_GREEN_SIZE, 8,
+            EGL_BLUE_SIZE, 8,
+            EGL_ALPHA_SIZE, 8,
+            EGL_DEPTH_SIZE, 24,
+            EGL_NONE
+    };
+    EGLConfig eglConfig;
+    EGLint numConfigs;
+    eglChooseConfig(eglDisplay,configAttribs,&eglConfig,1,&numConfigs);
+
+    // OpenGL ES 3.0 컨텍스트 생성
+    const EGLint contextAttribs[] = {
+            EGL_CONTEXT_CLIENT_VERSION, 3,
+            EGL_NONE
+    };
+    eglContext = eglCreateContext(eglDisplay,eglConfig,EGL_NO_CONTEXT,contextAttribs);
+
+    // EGLSurface 생성 (렌더링 대상, Window에 붙이기)
+    eglSurface = eglCreateWindowSurface(eglDisplay,eglConfig,window,nullptr);
+
+    // 컨텍스트와 서피스를 현재 스레드에 바인딩
+    eglMakeCurrent(
+            eglDisplay,
+            eglSurface,
+            eglSurface,
+            eglContext
+            );
+
+    // OpenGL 테스트 렌더링
+    glViewport(0,0,ANativeWindow_getWidth(window),ANativeWindow_getHeight(window));
+    glClearColor(0.1f,0.2f,0.3f,1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // 화면에 출력
+    eglSwapBuffers(eglDisplay,eglSurface);
 }
 extern "C"
 JNIEXPORT void JNICALL
@@ -23,5 +70,23 @@ Java_com_example_solarsystem_MainActivity_onSurfaceChanged(JNIEnv *env, jobject 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_example_solarsystem_MainActivity_onSurfaceDestroyed(JNIEnv *env, jobject thiz) {
-    // TODO: implement onSurfaceDestroyed()
+    if(eglDisplay != EGL_DEFAULT_DISPLAY){
+        eglMakeCurrent(
+                eglDisplay,
+                EGL_NO_SURFACE,
+                EGL_NO_SURFACE,
+                EGL_NO_CONTEXT
+        );
+        if(eglSurface != EGL_NO_SURFACE){
+            eglDestroySurface(eglDisplay,eglSurface);
+            eglSurface = EGL_NO_SURFACE;
+        }
+        if(eglContext != EGL_NO_CONTEXT) {
+            eglDestroyContext(eglDisplay, eglContext);
+            eglContext = EGL_NO_CONTEXT;
+        }
+
+        eglTerminate(eglDisplay);
+        eglDisplay = EGL_NO_DISPLAY;
+    }
 }
